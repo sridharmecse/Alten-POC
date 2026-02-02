fb47c45626334c26319bb8ba8ac735872574997f
mecse <mecsesridhar5389@gmail.com>
2026-02-02 17:09:24 +0530

    app settings updated

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -1,11 +1,1 @@
-/**
- * This file includes polyfills needed by Angular and is loaded before the app.
- * You can add your own extra polyfills to this file.
- *
- * The current setup is for modern evergreen browsers.
- */
-
-/***************************************************************************************************
- * Zone JS is required by Angular itself.
- */
 import 'zone.js';  // Included with Angular CLI.
